06a6122119dbfee98666c7ea53916267ae7760c5
William9923 <williamong1400@gmail.com>
2023-12-16 17:40:19 +0700

    improve autocompletion to suit my taste

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,6 +11,34 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
+    local kind_icons = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "",
+      Variable = "",
+      Class = "󰯲",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "",
+      Event = "",
+      Operator = "",
+      TypeParameter = "󰉺",
+    }
+
     local luasnip = require("luasnip")
     local cmp = require("cmp")
 
@@ -19,8 +47,8 @@ return {
         if cmp.visible() then
           -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
           cmp.select_next_item()
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- this way you will only jump inside the snippet region
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif has_words_before() then
@@ -39,5 +67,45 @@ return {
         end
       end, { "i", "s" }),
     })
+    opts.sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "nvim_lua" },
+      { name = "luasnip" },
+    }, {
+      { name = "path" },
+      { name = "buffer" },
+    })
+    opts.formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        vim_item.kind = kind_icons[vim_item.kind]
+        vim_item.menu = ({
+          nvim_lsp = "",
+          nvim_lua = "",
+          luasnip = "",
+          buffer = "",
+          path = "",
+          emoji = "",
+        })[entry.source.name]
+        return vim_item
+      end,
+    }
+    opts.enabled = function()
+      local context_status_ok, context = pcall(require, "cmp.config.context")
+      if not context_status_ok then
+        return true -- NOTE: by default, we should always apply auto-completion
+      end
+
+      -- Disable vimwiki filetype
+      if vim.bo.filetype == "vimwiki" then
+        return false
+      end
+
+      if vim.api.nvim_get_mode().mode == "c" then
+        return true
+      else
+        return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+      end
+    end
   end,
 }
