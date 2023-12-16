54552e56fb825b9964fac3da3490f105c379fb97
William9923 <williamong1400@gmail.com>
2023-12-16 21:23:56 +0700

    make the o button as opener

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,22 +2,89 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     cmd = "Neotree",
-    lazy = true,
+    lazy = false,
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
     opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      operjkiles_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      buffers = {
+        window = {
+          mappings = {
+            ["<space>"] = "none",
+            ["o"] = { "open_drop", nowait = true },
+            ["oc"] = "none",
+            ["od"] = "none",
+            ["og"] = "none",
+            ["om"] = "none",
+            ["on"] = "none",
+            ["os"] = "none",
+            ["ot"] = "none",
+          },
+        },
+      },
+      git_status = {
+        window = {
+          position = "float",
+          mappings = {
+            ["<space>"] = "none",
+            ["o"] = "none",
+            ["oc"] = "none",
+            ["od"] = "none",
+            ["og"] = "none",
+            ["om"] = "none",
+            ["on"] = "none",
+            ["os"] = "none",
+            ["ot"] = "none",
+          },
+        },
+      },
       window = {
         width = 25,
         mapping_options = {
           noremap = false,
-          nowait = true, -- NOTE: not wait for any default keymap associated with 'o'
+          nowait = true,
           silent = true,
         },
         mappings = {
           ["<space>"] = "none",
-          ["o"] = "open", -- NOTE: to make the behavior same with neotree
-          ["<CR>"] = "open", -- NOTE: to make the behavior same with neotree
+          ["o"] = { "open_drop", nowait = true },
+          ["oc"] = "none",
+          ["od"] = "none",
+          ["og"] = "none",
+          ["om"] = "none",
+          ["on"] = "none",
+          ["os"] = "none",
+          ["ot"] = "none",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
         },
       },
     },
+    config = function(_, opts)
+      opts.event_handlers = opts.event_handlers or {}
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
+        end,
+      })
+    end,
   },
   {
     "RRethy/vim-illuminate",
@@ -49,6 +116,31 @@ return {
         "DressingSelect",
         "TelescopePrompt",
       },
+    },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
+    end,
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
     },
   },
 }
