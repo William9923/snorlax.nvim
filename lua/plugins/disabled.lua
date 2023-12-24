e6d7c7cebed7259372a0825103e83a696017ae49
William Ong <william.ong@gojek.com>
2023-12-24 10:30:16 +0700

    add debugging capabilities (golang)

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   -- disable
-  { "folke/trouble.nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
   { "folke/which-key.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,19 +1,19 @@
 return {
-  "nvim-neotest/neotest",
-  opts = {
-    adapters = {
-      ["neotest-go"] = {},
-    },
-    status = { virtual_text = true },
-    output = { open_on_run = true },
-    quickfix = {
-      open = function()
-        if require("lazyvim.util").has("trouble.nvim") then
-          require("trouble").open({ mode = "quickfix", focus = false })
-        else
-          vim.cmd("copen")
-        end
-      end,
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          experimental = {
+            test_table = true,
+          },
+          args = { "-count=1", "-timeout=30s" },
+        },
+        ["neotest-plenary"] = {},
+      },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
     },
   },
+  { "nvim-neotest/neotest-plenary" },
 }
