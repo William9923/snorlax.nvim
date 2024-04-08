16abe4bddfebe049666240b7eca71e20dfb8be8a
William9923 <williamong1400@gmail.com>
2024-04-08 15:13:19 +0700

    enable autoformat on save for specific file type

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,4 @@
+local filetype = require "mason-lspconfig.mappings.filetype"
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
@@ -23,5 +24,20 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   callback = function()
     vim.cmd("hi LineNrAbove guifg=red ctermfg=red")
     vim.cmd("hi LineNrBelow guifg=cyan ctermfg=cyan")
+  end,
+})
+
+-- NOTE: apply auto format on specific file type
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go", -- autoformat on golang file
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.py", -- autoformat on python file
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
   end,
 })
