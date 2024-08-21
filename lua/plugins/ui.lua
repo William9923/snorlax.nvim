80b9f51c34747f42b6a7ea7c21eccb799802ce50
William Ong <william.ong@gojek.com>
2024-08-21 15:00:28 +0700

    don't hide cursorline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -81,9 +81,6 @@ return {
           },
         },
         window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           if vim.bo[props.buf].modified then
