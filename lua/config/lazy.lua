49de7d17178485a6b36d79d94f2444dbd60c38df
William Ong <william.ong@gojek.com>
2024-10-19 23:03:38 +0700

    add personal neovim plugin / extension

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -98,3 +98,14 @@ require("lazy").setup({
   },
   debug = false,
 })
+
+-- Personal neovim plugin / extension
+-- Dicipline: Don't repeat same key too much
+local dicipline = require("personal.dicipline")
+dicipline.norepeat() -- NOTE: prevent / train vim user to not apply continuous key repeat
+
+-- Keylogger: Showcase the actual key translated into neovim (vs what we type)
+local keylogger = require("personal.keylogger")
+keylogger.setup()
+keylogger.toggle() --> TODO: make a better way to activate this, for example can use autocmd
+
--- lua/config/personal.lua
+++
@@ -1,9 +0,0 @@
--- Dicipline: Don't repeat same key too much
-local dicipline = require("personal.dicipline")
-dicipline.norepeat() -- NOTE: prevent / train vim user to not apply continuous key repeat
-
--- Keylogger: Showcase the actual key translated into neovim (vs what we type)
-local keylogger = require("personal.keylogger")
-keylogger.setup()
--- keylogger.toggle() --> TODO: make a better way to activate this, for example can use autocmd
-
